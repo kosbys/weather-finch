17b936fa22a970e75167c86a23954b8919acf8b7
Kosbys <kosmicabysslord@protonmail.com>
2022-07-07 21:47:25 +0300

    Add fallback

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import domHandlers from './scripts/dom-handlers';
-import './styles/style.scss';
-
-domHandlers.createPage();
-
-// put nav and main into one single div?
-// margin left on searchbar
-// fahrenheit button
-// wiki url on click bird
