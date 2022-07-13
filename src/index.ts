a204de7e0103f9a6848fea1012b1bcba2103e50b
kosbys <93528140+kosbys@users.noreply.github.com>
2022-07-14 01:47:30 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,3 @@ import domHandlers from './scripts/dom-handlers';
 import './styles/style.scss';
 
 domHandlers.createPage();
-
-// put nav and main into one single div?
-// margin left on searchbar
-// fahrenheit button
-// wiki url on click bird
